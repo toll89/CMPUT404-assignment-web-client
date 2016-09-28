f3069d94bab8802567f210cfe8acf0b32608fe9f
toll89 <toll@ualberta.ca>
2016-09-28 12:16:45 -0600

    Update httpclient.py
    Modified to handle assignment 2.

--- httpclient.py
+++ httpclient.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+
 # coding: utf-8
+# Copyright 2016 Jarrett Toll
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +19,8 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+from urlparse import urlparse
+import re
 import sys
 import socket
 import re
@@ -28,27 +31,33 @@ def help():
     print "httpclient.py [GET/POST] [URL]\n"
 
 class HTTPResponse(object):
-    def __init__(self, code=200, body=""):
+    def __init__(self, code, body):
         self.code = code
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
-
     def connect(self, host, port):
-        # use sockets!
-        return None
+        clientsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientsock.connect((host, int(port)))
+        return clientsock
 
     def get_code(self, data):
-        return None
+        statusline = data.splitlines()[0]
+        statuscode = statusline.split()[1]
+        return statuscode
 
     def get_headers(self,data):
-        return None
+        statusheaders = re.split("\r\n\r\n", data)[0]
+        headers = statusheaders.partition("\r\n")[2]
+        return headers
 
     def get_body(self, data):
-        return None
+        if data:
+            body = re.split("\r\n\r\n", data)[1]
+        else:
+            body = None
+        return body
 
-    # read everything from the socket
     def recvall(self, sock):
         buffer = bytearray()
         done = False
@@ -61,14 +70,59 @@ class HTTPClient(object):
         return str(buffer)
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        urlpar = urlparse(url)
+        if "www." in urlpar.hostname:
+            host = urlpar.hostname[4:]
+        else:
+            host = urlpar.hostname
+
+        if urlpar.port:
+            port = urlpar.port
+        else:
+            port = 80
+
+        sock = HTTPClient().connect( host, port)
+        sock.send("GET %s HTTP/1.1\r\n" % urlpar.path)
+        sock.send("Host: %s\r\n" % host)
+        sock.send("Accept: */*\r\n\r\n")
+
+        response = self.recvall(sock)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        headers = self.get_headers(response)
+        return HTTPResponse(int(code), body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        urlpar = urlparse(url)
+        if "www." in urlpar.hostname:
+            host = urlpar.hostname[4:]
+        else:
+            host = urlpar.hostname
+        if urlpar.port:
+            port = urlpar.port
+        else:
+            port = 80
+        if args:
+            encoded = urllib.urlencode(args)
+            length = len(encoded)
+       
+        sockpost = HTTPClient().connect(host, port)
+        sockpost.send("POST %s HTTP/1.1\r\n" % urlpar.path)
+        if args:
+            sockpost.send("Content-Length: %d\r\n" % int(length))
+        else:
+            sockpost.send("Content-Length: 0\r\n")
+        sockpost.send("Host: %s\r\n" % host)
+        sockpost.send("Accept: */*\r\n")
+        sockpost.send("Content-Type: application/x-www-form-urlencoded\r\n")    
+        sockpost.send("\r\n")
+        if args:
+            sockpost.send(encoded)
+
+        response = self.recvall(sockpost)
+        code = self.get_code(response)
+        body = self.get_body(response)      
+        return HTTPResponse(int(code), body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
